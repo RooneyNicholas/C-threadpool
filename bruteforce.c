723423c412111c22e898656cef142d311327a3b9
Nicholas Rooney <nicholasrooney1993@hotmail.com>
2022-07-14 22:31:35 -0400

    added bruteforce to tracking

---
+++ bruteforce.c
@@ -0,0 +1,14 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <crypt.h>
+
+#include "bruteforce.h"
+#include "threadpool.h"
+
+void bruteforce(unsigned int num_threads, const char *encrypted_hash) {
+
+}
