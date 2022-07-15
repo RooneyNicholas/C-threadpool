9738787b10d68094b9df716b856f6e5c7b252184
Nicholas Rooney <nicholasrooney1993@hotmail.com>
2022-07-15 16:33:29 -0400

    Full score on autograder, tests only strings up to and including 8 characters, ensures null termination

--- bruteforce.c
+++ bruteforce.c
@@ -12,6 +12,8 @@
 /* Test Strings for quick reference to copy
 MD5 no salt -- key='1' - '$5$$wnhoDxNUJlXycXBX3TFylvsfBC5B3//3SrCpC50dqE8'
 MD5 salt=12345 -- key=1234 -- $5$12345$aBMyVPBFcg0A2cuP8s6ESkeqSpRL6VI8G6BOXUE8JA2 --> given example in assignment
+MD5 no salt -- key="" -- $5$$3c2QQ0KjIU1OLtB29cl8Fplc2WN7X89bnoEjaR7tWu.
+MD5 salt=112233 key=6834015 -- $5$112233$GOAACho98omkeguS3aGz19PO5QNBpvFCsjjGuR.l6YB
 */
 
 /*Begin prototypes*/
@@ -20,7 +22,7 @@ void decrypt_task(tpool_t, void*);
 
 typedef struct data {
     char *encrypted_hash;
-    char test_string[10]; //keeping the length of strings small to test
+    char test_string[9]; //8 max length + 1 null char
     int test_string_length; //current length of string being tested
 } data_t;
 
@@ -31,21 +33,23 @@ Avoids writing to the data_t args, to avoid any race condtions, keeping this fun
 */
 void decrypt_task(tpool_t pool, void* args) {
     data_t *data = (data_t*) args;
+    struct crypt_data cr_data; 
     // usleep(2000000);
     // printf("Current test string=%s -- crypt val=%s -- expected hash=%s\n", data->test_string, crypt(data->test_string, data->encrypted_hash), data->encrypted_hash );
-    if (strcmp(crypt(data->test_string, data->encrypted_hash), data->encrypted_hash) == 0) {
+    if (strcmp(crypt_r(data->test_string, data->encrypted_hash, &cr_data), data->encrypted_hash) == 0) {
         printf("%s\n", data->test_string);
         exit(0);
     }
 
     int string_length = data->test_string_length + 1;
-    for(int i = 0; i <= 9 && string_length <= 10; i++) {
+    for(int i = 0; i <= 9 && string_length <= 8; i++) {
         data_t *new_data = malloc(sizeof(struct data));
         new_data->test_string_length = string_length;
         new_data->encrypted_hash = malloc( (strlen(data->encrypted_hash)+1) * sizeof(char));
         strcpy(new_data->encrypted_hash, data->encrypted_hash);
         strcpy(new_data->test_string, data->test_string);
         new_data->test_string[string_length - 1] = i + '0';
+        new_data->test_string[string_length] = '\0'; //ensure null terminated string
         // printf("new_data string=%s test_string_length val=%d\n", new_data->test_string, new_data->test_string_length);
         tpool_schedule_task(pool, decrypt_task, (void*) new_data);
     }
@@ -55,13 +59,11 @@ void decrypt_task(tpool_t pool, void* args) {
 void bruteforce(unsigned int num_threads, const char *encrypted_hash) {
     tpool_t pool;
     pool = tpool_create(num_threads);
-    for(int i = 0; i <= 9; i++) {
-        data_t *data = malloc(sizeof(struct data));
-        data->encrypted_hash = malloc( (strlen(encrypted_hash)+1) * sizeof(char));
-        strcpy(data->encrypted_hash, encrypted_hash);
-        data->test_string[0] = '\0';
-        data->test_string_length = 0;
-        tpool_schedule_task(pool, decrypt_task, (void*) data);
-    }
+    data_t *data = malloc(sizeof(struct data));
+    data->encrypted_hash = malloc( (strlen(encrypted_hash)+1) * sizeof(char));
+    strcpy(data->encrypted_hash, encrypted_hash);
+    data->test_string[0] = '\0';
+    data->test_string_length = 0;
+    tpool_schedule_task(pool, decrypt_task, (void*) data);
     tpool_join(pool);
 }
